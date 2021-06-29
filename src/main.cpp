96997f43009930278b88ab6ffec524b0939f3422
Xart3misX <xart3misx@protonmail.com>
2021-06-29 17:15:23 +0200

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <ArduinoWebsockets.h>
 #include <ESP8266WiFi.h>
-#include <ArduinoJson.h>
 #include <Wire.h>
 #include <secrets.h>
 
@@ -10,26 +10,27 @@ const int CALIBRATION_BUFFER = 2000;
 const int capacity = JSON_OBJECT_SIZE(6);
 const uint8_t MPU6050SlaveAddress = 0x68;
 
-// sensitivity scale factor respective to full scale setting provided in datasheet 
+// sensitivity scale factor respective to full scale setting provided in
+// datasheet
 const uint16_t AccelScaleFactor = 16384;
 const uint16_t GyroScaleFactor = 131;
 
 // MPU6050 few configuration register addresses
-const uint8_t MPU6050_REGISTER_SMPLRT_DIV   =  0x19;
-const uint8_t MPU6050_REGISTER_USER_CTRL    =  0x6A;
-const uint8_t MPU6050_REGISTER_PWR_MGMT_1   =  0x6B;
-const uint8_t MPU6050_REGISTER_PWR_MGMT_2   =  0x6C;
-const uint8_t MPU6050_REGISTER_CONFIG       =  0x1A;
-const uint8_t MPU6050_REGISTER_GYRO_CONFIG  =  0x1B;
-const uint8_t MPU6050_REGISTER_ACCEL_CONFIG =  0x1C;
-const uint8_t MPU6050_REGISTER_FIFO_EN      =  0x23;
-const uint8_t MPU6050_REGISTER_INT_ENABLE   =  0x38;
-const uint8_t MPU6050_REGISTER_ACCEL_XOUT_H =  0x3B;
-const uint8_t MPU6050_REGISTER_SIGNAL_PATH_RESET  = 0x68;
+const uint8_t MPU6050_REGISTER_SMPLRT_DIV = 0x19;
+const uint8_t MPU6050_REGISTER_USER_CTRL = 0x6A;
+const uint8_t MPU6050_REGISTER_PWR_MGMT_1 = 0x6B;
+const uint8_t MPU6050_REGISTER_PWR_MGMT_2 = 0x6C;
+const uint8_t MPU6050_REGISTER_CONFIG = 0x1A;
+const uint8_t MPU6050_REGISTER_GYRO_CONFIG = 0x1B;
+const uint8_t MPU6050_REGISTER_ACCEL_CONFIG = 0x1C;
+const uint8_t MPU6050_REGISTER_FIFO_EN = 0x23;
+const uint8_t MPU6050_REGISTER_INT_ENABLE = 0x38;
+const uint8_t MPU6050_REGISTER_ACCEL_XOUT_H = 0x3B;
+const uint8_t MPU6050_REGISTER_SIGNAL_PATH_RESET = 0x68;
 
 int16_t AccelX, AccelY, AccelZ, Temperature, GyroX, GyroY, GyroZ;
 
-void I2C_Write(uint8_t deviceAddress, uint8_t regAddress, uint8_t data){
+void I2C_Write(uint8_t deviceAddress, uint8_t regAddress, uint8_t data) {
   Wire.beginTransmission(deviceAddress);
   Wire.write(regAddress);
   Wire.write(data);
@@ -38,29 +39,31 @@ void I2C_Write(uint8_t deviceAddress, uint8_t regAddress, uint8_t data){
 }
 
 // read all 14 register
-void Read_RawValue(uint8_t deviceAddress, uint8_t regAddress){
+void Read_RawValue(uint8_t deviceAddress, uint8_t regAddress) {
   Wire.beginTransmission(deviceAddress);
   Wire.write(regAddress);
   Wire.endTransmission();
   Wire.requestFrom(deviceAddress, (uint8_t)14);
-  AccelX = (((int16_t)Wire.read()<<8) | Wire.read());
-  AccelY = (((int16_t)Wire.read()<<8) | Wire.read());
-  AccelZ = (((int16_t)Wire.read()<<8) | Wire.read());
-  Temperature = (((int16_t)Wire.read()<<8) | Wire.read());
-  GyroX = (((int16_t)Wire.read()<<8) | Wire.read());
-  GyroY = (((int16_t)Wire.read()<<8) | Wire.read());
-  GyroZ = (((int16_t)Wire.read()<<8) | Wire.read());
+  AccelX = (((int16_t)Wire.read() << 8) | Wire.read());
+  AccelY = (((int16_t)Wire.read() << 8) | Wire.read());
+  AccelZ = (((int16_t)Wire.read() << 8) | Wire.read());
+  Temperature = (((int16_t)Wire.read() << 8) | Wire.read());
+  GyroX = (((int16_t)Wire.read() << 8) | Wire.read());
+  GyroY = (((int16_t)Wire.read() << 8) | Wire.read());
+  GyroZ = (((int16_t)Wire.read() << 8) | Wire.read());
 }
 
-//configure MPU6050
-void MPU6050_Init(){
+// configure MPU6050
+void MPU6050_Init() {
   delay(150);
   I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_SMPLRT_DIV, 0x07);
   I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_1, 0x01);
   I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_2, 0x00);
   I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_CONFIG, 0x00);
-  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_GYRO_CONFIG, 0x00);//set +/-250 degree/second full scale
-  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_CONFIG, 0x00);// set +/- 2g full scale
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_GYRO_CONFIG,
+            0x00); // set +/-250 degree/second full scale
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_CONFIG,
+            0x00); // set +/- 2g full scale
   I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_FIFO_EN, 0x00);
   I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_INT_ENABLE, 0x01);
   I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_SIGNAL_PATH_RESET, 0x00);
@@ -68,7 +71,7 @@ void MPU6050_Init(){
   delay(150);
 }
 
-String serializedSensorData; 
+String serializedSensorData;
 
 StaticJsonDocument<capacity> sensorJson;
 
@@ -77,42 +80,31 @@ bool isConnOpen = false;
 using namespace websockets;
 WebsocketsClient client;
 
-void onMessageCallback(WebsocketsMessage message)
-{
+void onMessageCallback(WebsocketsMessage message) {
   Serial.print("Got Message: ");
   Serial.println(message.data());
 }
 
-void onEventsCallback(WebsocketsEvent event, String data)
-{
-  if (event == WebsocketsEvent::ConnectionOpened)
-  {
+void onEventsCallback(WebsocketsEvent event, String data) {
+  if (event == WebsocketsEvent::ConnectionOpened) {
     Serial.println("Connnection Opened");
     isConnOpen = true;
-  }
-  else if (event == WebsocketsEvent::ConnectionClosed)
-  {
+  } else if (event == WebsocketsEvent::ConnectionClosed) {
     Serial.println("Connnection Closed");
     isConnOpen = false;
-  }
-  else if (event == WebsocketsEvent::GotPing)
-  {
+  } else if (event == WebsocketsEvent::GotPing) {
     Serial.println("Got a Ping!");
-  }
-  else if (event == WebsocketsEvent::GotPong)
-  {
+  } else if (event == WebsocketsEvent::GotPong) {
     Serial.println("Got a Pong!");
   }
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(921600);
   Wire.begin();
   MPU6050_Init();
   WiFi.begin(SECRET_SSID, SECRET_PASS);
-  for (int i = 0; i < 10 && WiFi.status() != WL_CONNECTED; i++)
-  {
+  for (int i = 0; i < 10 && WiFi.status() != WL_CONNECTED; i++) {
     Serial.print("+-+");
     delay(1000);
   }
@@ -134,24 +126,19 @@ void setup()
   client.ping();
 }
 
-void flashLed()
-{
-  if (ledState == LOW)
-  {
+void flashLed() {
+  if (ledState == LOW) {
     ledState = HIGH;
-  }
-  else
-  {
+  } else {
     ledState = LOW;
   }
   digitalWrite(LED_BUILTIN, ledState);
 }
 
-void loop()
-{
+void loop() {
   client.poll();
   Read_RawValue(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_XOUT_H);
-  
+
   /*
   double Ax, Ay, Az, Gx, Gy, Gz;
   Ax = (double)AccelX/AccelScaleFactor;
@@ -160,7 +147,7 @@ void loop()
   Gx = (double)GyroX/GyroScaleFactor;
   Gy = (double)GyroY/GyroScaleFactor;
   Gz = (double)GyroZ/GyroScaleFactor;
-  
+
   Serial.print("Ax: "); Serial.print(Ax);
   Serial.print(" Ay: "); Serial.print(Ay);
   Serial.print(" Az: "); Serial.print(Az);
@@ -174,7 +161,7 @@ void loop()
   sensorJson["Gx"] = GyroX;
   sensorJson["Gy"] = GyroY;
   sensorJson["Gz"] = GyroZ;
-  
+
   serializeJson(sensorJson, serializedSensorData);
 
   client.send(serializedSensorData);
